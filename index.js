ee97c57a685cd4e5237ade6af348dded7a27f41a
Sadjoaldi <alsannealdi@outlook.com>
2023-10-03 11:21:45 +0200

    chalk done

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+
+const devSenior = "Fred";
+const secondQ = "Charle";
+const thirdQ = "Nicolas";
+const fourthQ = "Salomon";
+
+console.log(chalk.green(devSenior));
+console.log(chalk.yellow(secondQ));
+console.log(chalk.red(thirdQ));
+console.log(chalk.blue(fourthQ));
